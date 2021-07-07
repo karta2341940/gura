ca0ce407608c8e0e596fb4ea7f79d8d5f7ff94f7
karta2341940 <a08487a@gmail.com>
2021-07-07 11:55:02 +0800

    list mobile ok

---
+++ mobile.js
@@ -0,0 +1,34 @@
+let ame = document.getElementsByClassName("block");
+let kiara=[];
+function gura(self)
+{
+    let calli = document.getElementsByClassName("list");
+    let ina=0;
+    for( i = 0 ; i <ame.length ; i ++)
+    {
+        if( ame[i] == self )
+        {
+            ina = i;
+            break;
+        }
+    }   
+    if( kiara[ina] != true )
+    {
+        calli[ina].classList.remove("clicked");
+        //calli[ina].classList.replace("");
+        kiara[ina] = true; 
+    }
+    else
+    {
+        calli[ina].classList.add("clicked");
+        //calli[ina].classList.remove("clicked");
+        kiara[ina] = false; 
+    }
+    //console.log(kiara[i]);
+}
+
+
+
+
+
+function Peko() {console.clear();}
