7d382425790bf7b7cced2cecfb54c9793e2f0c87
karta2341940 <a08487a@gmail.com>
2021-07-23 17:36:07 +0800

    slide animation ok

--- Test/mobile.js
+++ Test/mobile.js
@@ -1,27 +1,16 @@
 let ame  = document.getElementsByClassName("block");
 let kiara=[];
-let calli2 = document.getElementsByClassName("ls");
 let calli = document.getElementsByClassName("list");
 let ina=0;
-let heightpeko=[];                                      //save all value of height in this array
+let heightpeko=[];                          //save all value of height in this array
 var j = [];
 
-new function()
-{
-    
-    //var ss5 = getComputedStyle(calli2[5]).height;
-    for( let i = 0 ; i < ame.length ; i ++)
-    {
-        
-        heightpeko[i] = getComputedStyle(calli[i]).height;
-        calli[i].setAttribute("style","height: 0px");
-    }   
-}
+
 
 function gura(self)
 {
     
-    for( i = 0 ; i <ame.length ; i ++)
+    for( let i = 0 ; i <ame.length ; i ++)
     {
         if( ame[i] == self )
         {
@@ -33,12 +22,12 @@ function gura(self)
     
     if( kiara[ina] != true )
     {
-        Peko(kiara[ina],heightpeko[ina]);
+        calli[ina].setAttribute("style","height:"+heightpeko[ina]+";");
         kiara[ina] = true; 
     }
     else
     {
-        Peko(kiara[ina],0);
+        calli[ina].setAttribute("style","height:"+0+"px;");
         kiara[ina] = false; 
     }
 
@@ -58,8 +47,6 @@ function Peko(bool,pasen)
                 clearInterval(myvar);
             }
             calli[ina].setAttribute("style","height:"+j[ina]+"px");
-            //console.log("style : "+calli[ina].clientHeight);
-            //console.log("hei : "+heightpeko[ina]);
             j[ina]++
             
         }, 1);
@@ -75,15 +62,40 @@ function Peko(bool,pasen)
                 clearInterval(myvar);
             }
             calli[ina].setAttribute("style","height:"+j[ina]+"px");
-            //console.log("hei : "+j);
             j[ina]--;
             
         }, 1);
     }
         
 }
-
-function stopp()
+/*
+document.onresize=function()
 {
-    clearInterval(myvar);
+          
+    if(document.body.clientWidth <= 768)
+    {
+
+        for( let i = 0 ; i < ame.length ; i ++)
+        {
+                
+                heightpeko[i] = getComputedStyle(calli[i]).height;
+                calli[i].setAttribute("style","height: 0px");
+        }   
+    }
+            
+    
+}
+*/
+new function () 
+{
+    if(document.body.clientWidth <= 768)
+    {
+
+        for( let i = 0 ; i < ame.length ; i ++)
+        {
+                
+                heightpeko[i] = getComputedStyle(calli[i]).height;
+                calli[i].setAttribute("style","height: 0px");
+        }   
+    }
 }
