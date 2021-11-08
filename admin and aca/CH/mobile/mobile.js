d47ddc19fe35b936b54dfe8db729e7e1dc40e111
karta2341940 <a08487a@gmail.com>
2021-11-08 15:46:34 +0800

    update

--- admin and aca/CH/mobile/mobile.js
+++ admin and aca/CH/mobile/mobile.js
@@ -31,39 +31,6 @@ function gura()
     }
 
 }
-/*
-
-window.onresize=function()
-{
-    console.log("modify");
-         
-    if(document.body.clientWidth <= 768)
-    {
-        for( let i = 0 ; i < ame.length ; i ++)
-        {
-            ame[i].addEventListener("click",gura);
-        }   
-
-        for( let i = 0 ; i < ame.length ; i ++)
-        {
-            calli[i].setAttribute("style","height: auto;"); 
-            heightpeko[i] = getComputedStyle(calli[i]).height;
-            calli[i].setAttribute("style","height: 0px;");
-        }   
-        
-    }
-    else 
-    {
-        for( let i = 0 ; i < ame.length ; i ++)
-        {
-                calli[i].setAttribute("style","height: auto;"); 
-        }  
-    }
-    
-         
-    
-}
-*/
 new function () 
 {
     if(document.body.clientWidth <= 768)
